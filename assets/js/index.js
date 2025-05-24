6eaf3b882eec07938b1bc97f336ab1edd2554067
oibra <oibra@cs.washington.edu>
2025-05-24 12:53:37 -0500

    debug

--- assets/js/index.js
+++ assets/js/index.js
@@ -81,23 +81,26 @@ console.log('this is still working!')
                 }
 
                 const links = document.getElementsByClassName(page + '-btn')
-                links.forEach((link) => {
-                    link.onclick = (e) => {
-                        console.log(link)
-                        e.preventDefault()
-                        if (page != 'home') {
-                            history.pushState({page: page}, "", "/" + page)
-                        } else {
-                            history.pushState({page: page}, "", "/")
+                if (links) {
+                    links.forEach((link) => {
+                        link.onclick = (e) => {
+                            console.log(link)
+                            e.preventDefault()
+                            if (page != 'home') {
+                                history.pushState({page: page}, "", "/" + page)
+                            } else {
+                                history.pushState({page: page}, "", "/")
+                            }
+                            loadNewPage(page)
                         }
-                        loadNewPage(page)
-                    }
-                })
+                    })
+                }
             } catch (err) {
                 console.log(page)
+                console.log(err)
             }
-            
         }
+            
 
         // $$('/about-btn').onclick = (e) => {
         //     e.preventDefault()
